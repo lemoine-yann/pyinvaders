94fa39aceceade16146b85150a9ca66dad562ecf
Yann Lemoine <yann@cognitys.com>
2023-05-01 06:05:37 +0200

    first test with aliens

--- main.py
+++ main.py
@@ -2,13 +2,28 @@ import pgzrun
 import sys
 mod = sys.modules['__main__']  # fix dynamic references unresolved
 
-WIDTH = 800
-HEIGHT = 600
+# screen size
+WIDTH = 1920
+HEIGHT = 1080
+
+# init aliens array
+aliens = []
+for y in range(0, 4):  # 4 rows
+    for x in range(0, 10):  # 10 columns
+        aliens.append(mod.Actor('alienship', (x * 150 + 100, y * 100 + 100)))
 
 
 def draw():
     mod.screen.clear()
-    mod.screen.draw.circle((400, 300), 30, 'white')
+    for alien in aliens:  # draw each alien in array
+        alien.draw()
+
+
+def update():
+    for alien in aliens:  # move aliens
+        alien.left += 2
+        if alien.left > WIDTH:
+            alien.right = 0
 
 
 pgzrun.go()
